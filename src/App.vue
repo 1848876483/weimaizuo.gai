a0285dcb98d193be6e8d0b4b868ae0b85f936f56
1848876483 <1848876483@qq.com>
2018-05-11 09:37:05 +0800

    伪卖座.改

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="app" @tap="handleAllClick">
+      <mz-header></mz-header>
+      <mz-nav></mz-nav>
+      <keep-alive exclude="mz-detail">
+          <router-view></router-view>
+      </keep-alive>
+  </div>
+</template>
+
+<script>
+
+import axios from 'axios'
+import Header from './components/header.vue'
+import Nav from './components/nav.vue'
+
+axios.defaults.timeout = 2000;
+
+export default {
+  name: 'app',
+  data () {
+    return {
+      
+    }
+  },
+  components : {
+    [Header.name] : Header,
+    [Nav.name] : Nav
+  },
+  methods : {
+    handleAllClick(){
+      this.$store.commit('CHANGE_HEADER_SHOW',false);
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+*{ margin:0; padding:0; }
+html,body{ height: 100%; font-size:14px; background: #ebebeb; }
+li{ list-style:none; }
+img{ display:block; }
+</style>
---
+++ src/components/index.vue
@@ -0,0 +1,32 @@
+<template>
+	<div>
+		<mz-banner></mz-banner>
+		<mz-movie></mz-movie>
+	</div>
+</template>
+
+<script>
+
+import Banner from './banner.vue';
+import Movie from './movie.vue';
+
+	export default {
+		name : 'mz-index',
+		data(){
+			return {};
+		},
+		components : {
+			[Banner.name] : Banner,
+			[Movie.name] : Movie
+		},
+		mounted(){
+			
+		},
+		activated(){
+			this.$store.commit('CHANGE_HEADER_TITLE','卖座电影');
+		}
+	}
+</script>
+
+<style>
+</style>
